ca22115bedc260cce8b2f8fcdb19a3c39e5ccdfd
TKR <Cybertk7@gmail.com>
2022-04-08 10:57:33 +0200

    UpdateAPI *_*

--- CyberTKAPI/api.py
+++ CyberTKAPI/api.py
@@ -7,54 +7,61 @@ import json
 class API:
 
     def __init__(self, ApiKey, Version):
-        self._h = 'https://app.cybertkr.com'
+        self._h = 'https://api.cybertkr.com'
         self._get = httpx.Client(http2=True, timeout=120)
         self.api_key = ApiKey
         self.api_version = Version
+        self.api_versionFloat = "2.7"
         self.headers = {'ApiKey': self.api_key,'API-Version': self.api_version}
 
+    def CheckApiUpdate(self):
+        ver = self._get.get("https://github.com/CyberTKR/CyberTK-API/blob/master/CyberTKAPI/__version__").text.replace("\n"," ").replace(" ","")
+        print(ver)
+        if str(ver) != self.api_versionFloat:
+            print("\n\nPlease update the API : https://github.com/CyberTKR/CyberTK-API/\n\n")
+            
     def GoodPrint(self, jsonpack):
         print(json.dumps(jsonpack, indent=4,ensure_ascii=False))
 
     def _appuseragent(self, 
                       App):
-        istek = self._get.post(self._h + '/api/' + self.api_version
+        istek = self._get.post(self._h + '/' + self.api_version
                                + '/appname?App=' + App,
                                headers=self.headers).json()
         return istek
 
     def _apprandom(self):
-        istek = self._get.post(self._h + '/api/' + self.api_version
+        istek = self._get.post(self._h + '/' + self.api_version
                                + '/apprandom',
                                headers=self.headers).json()
         return istek
 
     def _weatherapi(self, location):
-        istek = self._get.post(self._h + '/api/' + self.api_version
+        istek = self._get.post(self._h + '/' + self.api_version
                                + '/weatherapi?Location=' + location,
                                headers=self.headers).json()
         return istek
 
     def _instaprofile(self, username):
-        istek = self._get.post(self._h + '/api/' + self.api_version
-                               + '/instagramapi/instasearch?Parameters='
+        istek = self._get.post(self._h + '/' + self.api_version
+                               + '/instagraminstasearch?Parameters='
                                 + username, headers=self.headers).json()
         return istek
 
     def _screenShotWeb(self, query):
-        istek = self._get.post(self._h + '/api/' + self.api_version
+        istek = self._get.post(self._h + '/' + self.api_version
                                + '/webscreenshot?Parameters=' + query,
                                headers=self.headers)
         return istek
 
     def _covid19(self, countrycode):
-        istek = self._get.post(self._h + '/api/' + self.api_version
+        istek = self._get.post(self._h + '/' + self.api_version
                                + '/covid19api?Parameters='
                                + countrycode, headers=self.headers)
         return istek
 
     def _tiktoksearch(self, username):
-        istek = self._get.post(self._h + '/api/' + self.api_version
+        istek = self._get.post(self._h + '/' + self.api_version
                                + '/tiktokapi?Parameters=' + username,
                                headers=self.headers)
         return istek
@@ -62,7 +69,7 @@ class API:
     def _lineqr(self, appname, UserAgent):
         self.headers['AppName'] = appname
         self.headers['UserAgent'] = UserAgent
-        istek = json.loads(self._get.post(self._h + '/api/'
+        istek = json.loads(self._get.post(self._h + '/'
                            + self.api_version + '/qrCode',
                            headers=self.headers).text)
         return istek
@@ -76,7 +83,7 @@ class API:
         ):
         self.headers['AppName'] = appname
         self.headers['UserAgent'] = userAgent
-        istek = json.loads(self._get.post(self._h + '/api/'
+        istek = json.loads(self._get.post(self._h + '/'
                            + self.api_version + '/Pincode' + '/' + key
                            + '/' + session, headers=self.headers).text)
         return istek
@@ -90,7 +97,7 @@ class API:
         ):
         self.headers['AppName'] = appname
         self.headers['UserAgent'] = userAgent
-        istek = json.loads(self._get.post(self._h + '/api/'
+        istek = json.loads(self._get.post(self._h + '/'
                            + self.api_version + '/authToken' + '/'
                            + key + '/' + session,
                            headers=self.headers).text)
@@ -101,7 +108,7 @@ class API:
         ):
         GetJsonPostDatas = json.dumps(getJsonPostData)
         self.headers['Content-Type'] = 'application/json'
-        istek = json.loads(self._get.post(self._h + '/api/'
+        istek = json.loads(self._get.post(self._h + '/'
                            + self.api_version + '/DeleteOtherFromChatALL',data=GetJsonPostDatas,
                            headers=self.headers).text)
         return istek
@@ -110,7 +117,7 @@ class API:
         self.headers['Phone'] = phone
         self.headers['CountryCode'] = country
         self.headers['IP'] = ip
-        istek = json.loads(self._get.post(self._h + '/api/'
+        istek = json.loads(self._get.post(self._h + '/'
                            + self.api_version + '/LINE/RegisterPrimaryService',
                            headers=self.headers).text)
         if istek["Status"] != 200:
@@ -121,7 +128,7 @@ class API:
         self.headers['Phone'] = phone
         self.headers['CountryCode'] = country
         self.headers['IP'] = ip
-        istek = json.loads(self._get.post(self._h + '/api/' + self.api_version + '/LINE/RegisterPrimaryService/'+ key + '/' + session + '/' + str(pin),headers=self.headers).text)
+        istek = json.loads(self._get.post(self._h + '/' + self.api_version + '/LINE/RegisterPrimaryService/'+ key + '/' + session + '/' + str(pin),headers=self.headers).text)
         if istek["Status"] != 200:
             raise Exception (istek["ErrorMessage"])
         return istek
