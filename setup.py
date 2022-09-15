e2cdf7ceefc3fe681b81d3b56c60b9fe193b5820
TKR <tolgkr@cybertkr.com>
2022-09-15 11:49:12 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ link = "https://github.com/CyberTKR/CyberTK-API"
 mail = "tolg@cybertkr.com"
 yazar = "CyberTK"
 tavsiye_py_vers = ">=3.6.0"
-version = "2.9"
+version = "2.9.2"
 install_Req = ["httpx==0.19.0", "httpx[http2]"]
 if not (sys.version_info.major == 3 and sys.version_info.minor >= 6):
     print("This script requires Python 3.6 or higher!")
