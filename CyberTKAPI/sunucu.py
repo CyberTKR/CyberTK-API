aab872e78f822afb11bdb2d788b7189f37f37dc6
TKR <Cybertk7@gmail.com>
2023-02-01 04:19:40 +0100

    *

--- CyberTKAPI/sunucu.py
+++ CyberTKAPI/sunucu.py
@@ -13,9 +13,9 @@ class ApiSunucu(object):
     async def bR3k9c(self,X2Qjs62,n2AN4n,d6YJV4,Hcl58y0=None,v291jqA=None):
         
         Y1fqJ12 = aiohttp.ClientSession()
-        async with Y1fqJ12 as TransPortSession:
-            ReqUrl = X2Qjs62+n2AN4n
-            async with TransPortSession.get(ReqUrl,headers=Hcl58y0, json=v291jqA) as E3k2Wi2:
+        async with Y1fqJ12 as xeLsT9E:
+            y37ycKC = X2Qjs62+n2AN4n
+            async with xeLsT9E.get(y37ycKC,headers=Hcl58y0, json=v291jqA) as E3k2Wi2:
                 if d6YJV4 == "text":
                     return await E3k2Wi2.text()
                 elif d6YJV4 == "json":
